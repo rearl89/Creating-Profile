96ca47f15650c58919b5fe661131b84618030e0d
Roger Earl <rogerearl@me.com>
2022-07-20 20:51:21 -0700

    re-added some content

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
 
 let content = document.querySelector('.js-generated.content')
 
-let header = document.createElement('h1')
-header.setAttribute('class', 'dog-name')
-header.append('Rizzo')
-content.append(header)
+//create header element
+let header = document.createElement("h1");
+header.setAttribute("class", "dog-name"); //<h1> class="dog-name"></h1>
+header.append("Rizzo"); //<h1 class="dog-name">Rizzo</h1>
+content.append(header);
 
-let dogContent = document.createElement('div')
-dogContent.setAttribute('class', 'dog-content')
-content.append(dogContent)
+//create dog-content
+let dogContent = document.createElement("div");
+dogContent.setAttribute("class", "dog-content");
+content.append(dogContent);
 
-let dogImage = document.createElement('img')
-dogImage.setAttribute('class', 'dog-image')
-dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
+//add dog-image
+let dogImage = document.createElement("img");
+dogImage.setAttribute("class", "dog-image");
+dogImage.setAttribute("src", "./assets/rizzo.jpg");
+dogContent.append(dogImage);
 
-let dogDetails = document.createElement('div')
-dogDetails.setAttribute('class', 'dog-details')
-content.append(dogDetails)
+//add dog-details div
+let dogDetails = document.createElement("div");
+dogDetails.setAttribute("class", "dog-details");
+dogContent.append(dogDetails);
 
+//add description h3
+let description = document.createElement("h3");
+description.textContent = "Description:";
+dogDetails.append(description);
+
+//add paragraph
+let para = document.createElement("p");
+para.textContent = "This gentle dog is aloof toward her owner, and never comes when called. She always acts as though any stranger she meets will harm her, and dislikes other animals.";
+dogDetails.append(para);
+
+//add feeding times h3
+let times = document.createElement("h3");
+times.textContent = "Feeding Times:";
+dogDetails.append(times);
+
+//add ul under times
+let ulist = document.createElement("ul");
+let time1 = document.createElement("li");
+time1.textContent = "9:00am";
+ulist.append(time1);
+let time2 = document.createElement("li");
+time2.textContent = "12:00pm";
+ulist.append(time2);
+let time3 = document.createElement("li");
+time3.textContent = "5:00pm";
+ulist.append(time3);
+dogDetails.append(ulist);
 
